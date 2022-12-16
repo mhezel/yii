aa913d47bdc1936577c9000e1bab7109defa2acd
Mhezel Mohammad <mhezelkhan@gmail.com>
2022-12-16 08:43:31 +0800

    commit changes

---
+++ advanced/frontend/models/Projects.php
@@ -0,0 +1,53 @@
+<?php
+namespace app\models;
+use Yii;
+
+/**
+ * This is the model class for table "tb_projects".
+ *
+ * @property int $id
+ * @property string|null $posted_by
+ * @property string|null $title
+ * @property string|null $body
+ * @property string|null $image
+ * @property string $created_at
+ */
+class Projects extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'tb_projects';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [[ 'title','body'], 'required'],
+            [['body'], 'string'],
+            [['created_at','posted_by','image'], 'safe'],
+            [['posted_by', 'title'], 'string', 'max' => 100],
+            [['image'], 'string', 'max' => 200],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'posted_by' => 'Posted By',
+            'title' => 'Title',
+            'body' => 'Body',
+            'image' => 'Image',
+            'created_at' => 'Created At',
+        ];
+    }
+}
