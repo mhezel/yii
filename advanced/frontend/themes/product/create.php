1234fe628412d71532189e9c4c2d7f1c4b5de745
Mhezel Mohammad <mhezelkhan@gmail.com>
2023-01-03 10:01:12 +0800

    2nd commit - 1/3/2023
    file extensions - yii2-tinymce-widget Renders a TinyMCE WYSIWYG text editor plugin widget

--- advanced/frontend/themes/product/create.php
+++ advanced/frontend/themes/product/create.php
@@ -2,14 +2,25 @@
 /** @var yii\web\View $this */
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use dosamigos\tinymce\TinyMce;
 ?>
-
 <h1>Submit your own Project Entry! </h1>
 <div class="container-fluid">
-
 <?php $form = ActiveForm::begin(['options'=>['enctype'=>'multipart/form-data']])?>
 <?= $form->field($model,'title')->textInput()?>
-<?= $form->field($model,'body')->textarea(['rows'=>5])?>
+<?php //$form->field($model,'body')->textarea(['rows'=>5])*/ ?>
+    <?= $form->field($model, 'body')->widget(TinyMce::className(), [
+        'options' => ['rows' => 6],
+        'language' => 'en',
+        'clientOptions' => [
+            'plugins' => [
+                "advlist autolink lists link charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste"
+            ],
+            'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        ]
+    ]);?>
 <?= $form->field($model,'image')->fileInput();?><br>
 <?=Html::submitButton('Submit',['class'=>'btn btn-primary'])?>
 <?php ActiveForm::end()?>
--- advanced/frontend/themes/projects/create.php
+++ advanced/frontend/themes/projects/create.php
@@ -2,6 +2,7 @@
 /** @var yii\web\View $this */
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use dosamigos\tinymce\TinyMce;
 ?>
 <h1>Submit your own Project Entry! </h1>
 
@@ -9,12 +10,23 @@ use yii\widgets\ActiveForm;
 
 <?php $form = ActiveForm::begin(['options'=>['enctype'=>'multipart/form-data']])?>
 <?= $form->field($model,'title')->textInput()?>
-<?= $form->field($model,'body')->textarea(['rows'=>5])?>
+<?php //$form->field($model,'body')->textarea(['rows'=>5])*/ ?>
+<?= $form->field($model, 'body')->widget(TinyMce::className(), [
+            'options' => ['rows' => 6],
+            'language' => 'en',
+            'clientOptions' => [
+                'plugins' => [
+                    "advlist autolink lists link charmap print preview anchor",
+                    "searchreplace visualblocks code fullscreen",
+                    "insertdatetime media table contextmenu paste"
+                ],
+                'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+            ]
+]);?>
 <?= $form->field($model,'image')->fileInput();?><br>
-<?php/* $form->field($model, 'image')->widget(FileInput::classname(), [
+<?php /*$form->field($model, 'image')->widget(FileInput::classname(), [
     'options' => ['accept' => 'image/*'],
 ]);*/?>
 <?=Html::submitButton('Submit',['class'=>'btn btn-primary'])?>
 <?php ActiveForm::end()?>
-
 </div>
